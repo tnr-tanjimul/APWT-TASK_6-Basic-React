7239e38aa907deea4eefc6d9c7afe74048ba7843
MD Tanjimul Islam <67940292+tnr-tanjimul@users.noreply.github.com>
2021-11-24 11:27:39 +0600

    add student list pages

--- src/AppMain.jsx
+++ src/AppMain.jsx
@@ -1,22 +1,43 @@
 import LeftSideBar from "./Components/LeftSidebar";
 import Dashboard from "./Components/Pages/Dashboard";
+import StudentList from "./Components/Pages/StudentList";
 import RightSidebar from "./Components/RightSidebar";
 import TopBar from "./Components/TopBar";
+import { BrowserRouter as Router, Route, Switch,useParams } from 'react-router-dom'
+import StudentDetails from "./Components/Pages/StudentDetails";
+function AppMain() {
+    return (
+        <Router>
+            <>
+                {/* Begin page */}
+                <div id="wrapper">
+                    <TopBar></TopBar>
+                    <LeftSideBar></LeftSideBar>
+                    
 
-function AppMain(){
-    return(
-        <>
-        {/* Begin page */}
-        <div id="wrapper">
-			<TopBar></TopBar>
-            <LeftSideBar></LeftSideBar>
-            <Dashboard></Dashboard>
-		</div>
 
-        <RightSidebar></RightSidebar>
-        <div className="rightbar-overlay"></div>
-        {/* END wrapper */}
-        </>
+                    <Switch>
+                        <Route exact path="/">
+                            <Dashboard></Dashboard>
+                        </Route>
+                        <Route exact path="/student-list">
+                            <StudentList></StudentList>
+                        </Route>
+
+                        <Route exact path="/student/:id">
+                            <StudentDetails/>
+                        </Route>
+                    </Switch>
+
+
+
+                </div>
+
+                <RightSidebar></RightSidebar>
+                <div className="rightbar-overlay"></div>
+                {/* END wrapper */}
+            </>
+        </Router>
     );
 }
 export default AppMain;
